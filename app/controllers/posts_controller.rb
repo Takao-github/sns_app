ad1391c64b455003f76e803079c7a2eab743b8f0
takao <takao.shimose01@gmail.com>
2023-08-01 15:41:23 +0000

    13nolast

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,5 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
-
   def index
     @title = params[:title]
     if @title.present?
@@ -9,8 +8,8 @@ class PostsController < ApplicationController
       @posts = Post.all
     end
     render :index
-  end 
-  # ここから
+  end
+
   def new
     @post = Post.new
     render :new
@@ -24,15 +23,38 @@ class PostsController < ApplicationController
     end
 
     if @post.save
-      redirect_to index_post_path, notice: '登録しました'
+      redirect_to index_post_path, notice: '投稿しました'
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  # ここから追加
+  def edit
+    @post = Post.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if params[:post][:image]
+      @post.image.attach(params[:post][:image])
+    end
+    if @post.update(post_params)
+      redirect_to index_post_path, notice: '更新しました'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  # ここまで
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to index_post_path, notice: '削除しました'
+  end
+  
   private
   def post_params
     params.require(:post).permit(:title, :body, :image)
   end
-  # ここまで
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,18 +4,23 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  # これです
   get '/', to: 'posts#index', as: 'index_post'
 
   # PostsController
   get 'posts/new', to: 'posts#new', as: 'new_post'
   post 'posts/new', to: 'posts#create', as: 'create_post'
+  # ここから追加
+  get 'posts/edit/:id', to: 'posts#edit', as: 'edit_post'
+  post 'posts/edit/:id', to: 'posts#update', as: 'update_post'
+  delete 'posts/destroy/:id', to: 'posts#destroy', as: 'destroy_post'
+  
+  get 'posts/show/:post_id/comments/new', to: 'comments#new', as: 'new_comment'
+  post 'posts/show/:post_id/comments/new', to: 'comments#create', as: 'create_commet'
 
   # TopicsController
-  get 'topics', to: 'topics#index', as: 'topics'
   get 'topics/new', to: 'topics#new', as: 'new_topic'
-  post '/topics/new', to: 'topics#create', as: 'create_topic'
-  get 'topics/edit/:id', to: 'topics#edit', as: 'edit_topic'
-  post '/topics/edit/:id', to: 'topics#update', as: 'update_topic'
+  post '/topics/create', to: 'topics#create', as: 'create_topic'
+  get 'topics/edit', to: 'topics#edit', as: 'edit_topic'
+  post '/topics/update', to: 'topics#update', as: 'update_topic'
 
 end
